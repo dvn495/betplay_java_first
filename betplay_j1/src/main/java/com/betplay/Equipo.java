ed7b0f8097285fae516832677d777e7ec77d99c4
Nicolas Pedraza Maldonado <152308538+dvn495@users.noreply.github.com>
2024-05-28 09:03:47 -0500

    Add files via upload

--- betplay_j1/src/main/java/com/betplay/Equipo.java
+++ betplay_j1/src/main/java/com/betplay/Equipo.java
@@ -1,20 +1,20 @@
 package com.betplay;
 
 public class Equipo {
-    String name;
-    int partidos_jugados = 0;
-    int partidos_ganados = 0;
-    int partidos_perdidos = 0;
-    int partidos_empatados = 0;
-    int goles_favor = 0;
-    int goles_contra = 0;
-    int total_puntos = 0;
+    private String name;
+    private int partidos_jugados;
+    private int partidos_ganados;
+    private int partidos_perdidos;
+    private int partidos_empatados;
+    private int goles_favor;
+    private int goles_contra;
+    private int total_puntos;
 
-    public Equipo(){
+    public Equipo() {
         
     }
 
-    public Equipo(String name, int partidos_jugados, int partidos_ganados, int partidos_perdidos, int partidos_empatados, int goles_favor, int goles_contra, int total_puntos) {
+    public Equipo(String name, int partidos_jugados, int partidos_ganados, int partidos_perdidos,int partidos_empatados, int goles_favor, int goles_contra, int total_puntos) {
         this.name = name;
         this.partidos_jugados = partidos_jugados;
         this.partidos_ganados = partidos_ganados;
@@ -24,7 +24,7 @@ public class Equipo {
         this.goles_contra = goles_contra;
         this.total_puntos = total_puntos;
     }
-    
+
     public String getName() {
         return name;
     }
@@ -32,6 +32,60 @@ public class Equipo {
         this.name = name;
     }
 
-    
+    public int getPartidos_jugados() {
+        return partidos_jugados;
+    }
 
+    public void setPartidos_jugados(int partidos_jugados) {
+        this.partidos_jugados = partidos_jugados;
+    }
+
+    public int getPartidos_ganados() {
+        return partidos_ganados;
+    }
+
+    public void setPartidos_ganados(int partidos_ganados) {
+        this.partidos_ganados = partidos_ganados;
+    }
+
+    public int getPartidos_perdidos() {
+        return partidos_perdidos;
+    }
+
+    public void setPartidos_perdidos(int partidos_perdidos) {
+        this.partidos_perdidos = partidos_perdidos;
+    }
+
+    public int getPartidos_empatados() {
+        return partidos_empatados;
+    }
+
+    public void setPartidos_empatados(int partidos_empatados) {
+        this.partidos_empatados = partidos_empatados;
+    }
+
+    public int getGoles_favor() {
+        return goles_favor;
+    }
+
+    public void setGoles_favor(int goles_favor) {
+        this.goles_favor = goles_favor;
+    }
+
+    public int getGoles_contra() {
+        return goles_contra;
+    }
+
+    public void setGoles_contra(int goles_contra) {
+        this.goles_contra = goles_contra;
+    }
+
+    public int getTotal_puntos() {
+        return total_puntos;
+    }
+
+    public void setTotal_puntos(int total_puntos) {
+        this.total_puntos = total_puntos;
+    }
+        
 }
--- betplay_j1/src/main/java/com/betplay/Main.java
+++ betplay_j1/src/main/java/com/betplay/Main.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("Error al limpiar la pantalla: " + e.getMessage());
+        }
+    } 
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -14,47 +27,103 @@ public class Main {
 
         while (isActive) {
 
+            clearScreen();
+
             System.out.println("---LIGA BETPLAY---");
             System.out.println(" 1. REGISTRAR UN EQUIPO\n 2. REGISTRAR UNA FECHA\n 3. REPORTES\n 4. DETENER LA EJECUCION");
 
-            int election = (Integer.parseInt(sc.nextLine()));
-    
-            if (election == 1) {
-    
-                boolean isActiveTeams = true;
-    
-                while (isActiveTeams) {
-                    Equipo myEquipo = new Equipo();
 
-                    System.out.println("MENU REGISTRO EQUIPO\n  NOMBRE DEL EQUIPO: ");
-                    myEquipo.setName(sc.nextLine());
+            try {
 
-                    equipos.add(myEquipo);
+                int election = (Integer.parseInt(sc.nextLine()));
 
-                    /* for (Equipo equipo :equipos) {
-                        System.out.println(equipo.getName());
-                    } */
 
-                    boolean isActiveTeamsSelection = true;
+                switch (election) {
 
-                    while (isActiveTeamsSelection) {
-                        System.out.println("¿DESEA REGISTRAR OTRO EQUIPO?\n 1. SI\n 2. NO");
-                        String electionTeams = sc.nextLine();
-                        if (electionTeams.equals("1")) {
-                            isActiveTeams = true;
-                            isActiveTeamsSelection = false;
-                        }
-                        else if (electionTeams.equals("2")) {
-                            isActiveTeams = false;
-                            isActiveTeamsSelection = false;
-                        } 
-                        else {
-                            System.out.println("OPCION NO VALIDA");
+                    case 1 ->{
+
+                        boolean isActiveTeams = true;
+            
+                        while (isActiveTeams) {
+        
+                            clearScreen();
+        
+                            Equipo myEquipo = new Equipo();
+        
+                            System.out.println("MENU REGISTRO EQUIPO\n  NOMBRE DEL EQUIPO: ");
+                            myEquipo.setName(sc.nextLine());
+                            myEquipo.setPartidos_jugados(0);
+                            myEquipo.setPartidos_ganados(0);
+                            myEquipo.setPartidos_perdidos(0);
+                            myEquipo.setPartidos_empatados(0);
+                            myEquipo.setGoles_favor(0);
+                            myEquipo.setGoles_contra(0);
+                            myEquipo.setTotal_puntos(0);
+                            equipos.add(myEquipo);
+                            System.out.println("\nEQUIPO REGISTRADO EXITOSAMENTE\n Presione cualquier tecla para continuar...");
+                            sc.nextLine();
+
+                            /* for (Equipo equipo :equipos) {
+                                System.out.println(equipo.getName());
+                            } */
+        
+                            boolean isActiveTeamsSelection = true;
+        
+                            while (isActiveTeamsSelection) {
+                                clearScreen();
+                                System.out.println("¿DESEA REGISTRAR OTRO EQUIPO?\n 1. SI\n 2. NO");
+                                String electionTeams = sc.nextLine();
+                                
+                                try {
+                                    int option = Integer.parseInt(electionTeams);
+                                    
+                                    switch (option) {
+                                        case 1 -> {
+                                            isActiveTeams = true;
+                                            isActiveTeamsSelection = false;
+                                        }
+                                        case 2 -> {
+                                            isActiveTeams = false;
+                                            isActiveTeamsSelection = false;
+                                        }
+                                        default -> {
+                                            System.out.println("OPCION NO VALIDA\n Presione cualquier tecla para continuar...");
+                                            sc.nextLine();
+                                        }
+                                    }
+                                } catch (NumberFormatException e) {
+                                    System.out.println("Por favor, ingrese un número válido.\n Presione cualquier tecla para continuar...");
+                                    sc.nextLine();
+                                }
+                            }
                         }  
-                    }  
+                    }
+
+                    case 2 -> {
+                        clearScreen();
+                        if (equipos.size() >= 2) {
+                            boolean isActiveDate = true;
+
+                            while (isActiveDate) {
+                                
+                            }
+                        } else {
+                            System.out.println("NO HAY SUFICIENTES EQUIPOS REGISTRADOS\n Presione cualquier tecla para continuar...");
+                            sc.nextLine();
+                        }
+
+                    }
+
+                    default -> {
+                        System.out.println("OPCION NO VALIDA\n Presione cualquier tecla para continuar...");
+                        sc.nextLine();
+                    }
                 }
                 
-            }
+            }   catch (NumberFormatException e) {
+                System.out.println("Por favor, ingrese un número válido.\n Presione cualquier tecla para continuar...");
+                sc.nextLine();
+            }        
         }
     }
 }
